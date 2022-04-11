e467a112ae2b50c6e61f875fc1fbe406ff3de547
guochao <463561248@qq.com>
2022-04-11 16:05:51 +0800

    ✨ feat:  了解和学习nextjs界面导航和静态资源

---
+++ pages/_app.js
@@ -0,0 +1,12 @@
+/*
+ * @Author: saber
+ * @Date: 2022-04-11 15:45:32
+ * @LastEditTime: 2022-04-11 15:47:12
+ * @LastEditors: saber
+ * @Description: 
+ */
+import '../styles/global.css'
+
+export default function App({ Component, pageProps }) {
+  return <Component {...pageProps} />
+}
