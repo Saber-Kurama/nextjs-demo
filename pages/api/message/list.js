d4201433617080cf58c5214a25082659a9723f5a
guochao <463561248@qq.com>
2022-04-12 00:18:17 +0800

    ✨ feat:  调整swagger 的文档

--- pages/api/message/list.js
+++ pages/api/message/list.js
@@ -1,23 +1,47 @@
 /**
-   * @swagger
-   * tags:
-   *   name: message
-   *   description: 消息
-   */
+ * @swagger
+ * tags:
+ *   name: message
+ *   description: 消息
+ */
 /**
  * @swagger
- * /message/list:
- *   get:
- *     tags:
- *        - message
- *     description: 获取消息列表
- *     responses:
- *       200:
- *         description: hello world
+ *
+ * components:
+ *   links:
+ *     UserRepositories:
+ *       operationId: getRepositoriesByOwner
+ *       parameters:
+ *         username: '$response.body#/username'
+ *   schemas:
+ *     user:
+ *        type: object
+ *        properties:
+ *          username:
+ *            type: string
+ *            description: '用户名'
+ *          uuid:
+ *            type: string
  */
- const handler = (_req, res) => {
+
+/**
+ * @swagger
+ *  /api/message/list:
+ *    get:
+ *      tags:
+ *        - message
+ *      description: 获取消息列表
+ *      responses:
+ *        200:
+ *          description: OK
+ *          content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/user'
+ */
+const handler = (_req, res) => {
   res.status(200).json({
-    result: 'hello world',
+    result: "hello world",
   });
 };
 
